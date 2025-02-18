f8c1ff1ccaadf26a9e9e4c646868204fd77d6582
JCRD05 <josecarlosruizdiaz05@gmail.com>
2025-02-18 13:25:26 -0600

    Update main.cs

--- main.cs
+++ main.cs
@@ -32,6 +32,7 @@ class Program
     static void Operations(list lista, int opt)
     {
         string data;
+        int index;
         switch(opt)
         {
             case 1: 
@@ -57,14 +58,22 @@ class Program
                 lista.PrintBackwards(); // Imprime los elementos de la lista en orden inverso
                 break;
             case 7: // Inserta un elemento a un punto dado de la lista
-                Console.WriteLine("Introduzca el texto que desea meter al final de la lista\n");
+                Console.WriteLine("Introduzca el texto que desea meter en el punto dado de la lista\n");
                 data = Console.ReadLine();
                 Console.WriteLine("¿En que posicion lo quiere colocar?\n");
-                int index = Convert.ToInt32(Console.ReadLine());
+                index = Convert.ToInt32(Console.ReadLine());
                 lista.Insert(data, index);
                 break;
+            case 8: // Borra un elemento de un punto dado de la lista
+                Console.WriteLine("¿En que posicion quiere eliminar el dato?\n");
+                index = Convert.ToInt32(Console.ReadLine());
+                lista.Delete(index);
+                break;
+            case 9:
+                Console.WriteLine("Gracias Por Usar El Programa");
+                break;
             default: 
-                Console.WriteLine("Elija una opcion entre 1 y 7"); // Mensaje de error en caso de opción inválida
+                Console.WriteLine("Elija una opcion entre 1 y 8"); // Mensaje de error en caso de opción inválida
                 break;
         }
     }
@@ -75,51 +84,54 @@ class Program
 
         // Pregunta al usuario si desea crear una lista
 		Console.WriteLine("¿Desea crear una lista?\n");
-		Console.WriteLine("1. Si 2. No\n");
-		int makeList = Convert.ToInt32(Console.ReadLine());
-		if(makeList == 2) {return;} // Saca al usuario del programa
-		
-		// Pregunta al usuario si la lista será circular
-		Console.WriteLine("¿Desea que sea circular?\n");
-		Console.WriteLine("1. Si 2. No\n");
-		int isCircular = Convert.ToInt32(Console.ReadLine());
-		
-		// Pide el nombre de la lista
-		Console.WriteLine("Introduzca el nombre de la lista\n");
-		string listName = Console.ReadLine();
-		
-		// Define si la lista será circular o no según la opción elegida
-		bool Circular;
-		if(makeList == 1 && isCircular == 1)
-		{
-		    Circular = true;
-		}
-		else if(makeList == 1 && isCircular == 2)
-		{
-		    Circular = false;
-		}
-		else 
-		{
-		    return; // Si el usuario no quiere crear una lista, el programa termina
-		}
-		
-		// Crea la lista con el nombre y tipo (circular o no) definido por el usuario
-		list lista = new list(listName, Circular);
-		
-		// Bucle que permite al usuario elegir operaciones sobre la lista hasta que decida salir
-		do
-		{
-		    Console.WriteLine("¿Que operacion desea hacer?\n");
-		    Console.WriteLine("1. Agregar un elemento al final\n");
-		    Console.WriteLine("2. Agregar un elemento al principio\n");
-		    Console.WriteLine("3. Eliminar un elemento al final\n");
-		    Console.WriteLine("4. Eliminar un elemento al principio\n");
-		    Console.WriteLine("5. Imprimir elementos\n");
-		    Console.WriteLine("6. Imprimir elementos al revés\n");
-		    Console.WriteLine("7. Insertar un elemento");
-		    
-		    opt = Convert.ToInt32(Console.ReadLine());
-		    Operations(lista, opt); // Ejecuta la operación seleccionada
-		} while (Exit() && (opt > 0 && opt < 8)); // Repite mientras el usuario quiera continuar y la opción sea válida
+        Console.WriteLine("1. Si 2. No\n");
+        int makeList = Convert.ToInt32(Console.ReadLine());
+        if(makeList == 2) {return;} // Saca al usuario del programa
+
+        // Pregunta al usuario si la lista será circular
+        Console.WriteLine("¿Desea que sea circular?\n");
+        Console.WriteLine("1. Si 2. No\n");
+        int isCircular = Convert.ToInt32(Console.ReadLine());
+
+        // Pide el nombre de la lista
+        Console.WriteLine("Introduzca el nombre de la lista\n");
+        string listName = Console.ReadLine();
+
+        // Define si la lista será circular o no según la opción elegida
+        bool Circular;
+        if(makeList == 1 && isCircular == 1)
+        {
+            Circular = true;
+        }
+        else if(makeList == 1 && isCircular == 2)
+        {
+            Circular = false;
+        }
+        else 
+        {
+            return; // Si el usuario no quiere crear una lista, el programa termina
+        }
+
+        // Crea la lista con el nombre y tipo (circular o no) definido por el usuario
+        list lista = new list(listName, Circular);
+        
+        // Bucle que permite al usuario elegir operaciones sobre la lista hasta que decida salir
+        do
+        {
+            Console.WriteLine("¿Que operacion desea hacer?\n");
+            Console.WriteLine("1. Agregar un elemento al final\n");
+            Console.WriteLine("2. Agregar un elemento al principio\n");
+            Console.WriteLine("3. Eliminar un elemento al final\n");
+            Console.WriteLine("4. Eliminar un elemento al principio\n");
+            Console.WriteLine("5. Imprimir elementos\n");
+            Console.WriteLine("6. Imprimir elementos al revés\n");
+            Console.WriteLine("7. Insertar un elemento\n");
+            Console.WriteLine("8. Borrar un elemento\n");
+            Console.WriteLine("9. Salir Del Programa");
+            
+            opt = Convert.ToInt32(Console.ReadLine());
+            Operations(lista, opt); // Ejecuta la operación seleccionada
+            if (opt == 9){ return; }
+        } while (Exit() && (opt > 0 && opt < 9)); // Repite mientras el usuario quiera continuar y la opción sea válida
 	}
 }
