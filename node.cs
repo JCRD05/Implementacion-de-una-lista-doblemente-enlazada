a43c8330bd8f16660b95cb8b536481e1c1e7e9cb
JCRD05 <josecarlosruizdiaz05@gmail.com>
2025-02-21 20:37:14 -0600

    Update node.cs

--- node.cs
+++ node.cs
@@ -1,45 +1,39 @@
 using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace List 
+namespace DataStructures
 {
-	class node
-	{
-	    // Atributo que almacena el dato del nodo
-	    public string data; 
-	    
-	    // Referencia al nodo anterior en la lista doblemente enlazada
-	    public node prev; 
-	    
-	    // Referencia al nodo siguiente en la lista doblemente enlazada
-	    public node next; 
-	    
-	    // Constructor que inicializa el nodo con un dato y referencias a nodos previos y siguientes
-	    public node(string data, node prev, node next)
-	    {
-	        this.data = data;
-	        this.prev = prev;
-	        this.next = next;
-	    }
-	    
-	    // Constructor que inicializa el nodo solo con el dato, dejando las referencias en null
-	    public node(string data)
-	    {
-	        this.data = data;
-	        this.prev = null;
-	        this.next = null;
-	    }
-	    
-	    // Constructor por defecto que inicializa el nodo con un dato vacío y referencias en null
-	    public node()
-	    {
-	        this.data = " ";
-	        this.prev = null;
-	        this.next = null;
-	    }
-	}
+    class Node
+    {
+        public object data; // Dato que almacena el nodo
+        public Node next; // Direccion de memoria del nodo siguiente
+        public Node prev; // Direccion de memoria del nodo anterior
+        
+        public Node(object data, Node next, Node prev)
+        {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+        
+        // Constructores de los nodos
+        public Node(object data)
+        {
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+        
+        public Node()
+        {
+            data = null;
+            next = null;
+            prev = null;
+        }
+    }
 }
